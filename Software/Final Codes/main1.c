beda2c73e0ebf1cd231b35df29e83b0ce7726e37
ChristineZhao <cz64@duke.edu>
2016-11-14 10:25:53 -0500

    Learning period

--- Software/Final Codes/main1.c
+++ Software/Final Codes/main1.c
@@ -25,24 +25,23 @@ int oldRawV1; // old raw signal from lead 1
 int oldRawV2; // old raw signal from lead 2
 int oldV1; // old processed signal from lead 1
 int oldV2; // old processed signal from lead 2
-int thres1; // threshold for voltage classification
-int thres2; // threshold for voltage classification
+int thres1; // magnitude threshold
+int thres2; // magnitude threshold
+int rslope1; // rising slope threshold
+int dslope1; // descending slope threshold
+int rslope2; // rising slope threshold
+int dslope2; // descending slope threshold
 int hyst; // hysteresis range
 int state;
-int max1;
-int min1;
-int max2;
-int min2;
-int recComm; // received signal via communication
 int t; // sampling period (x10^-2 seconds)
 
 //Function definitions
 void SysInit(void);
+void Calibrate(void);
 int GetData(int channel);
 int Debounce(int raw, int oldraw, int olddeb, int channel);
 int Decode(int channel1, int channel2);
 void Transmit(int info);
-void Calibrate(void);
 
 void main(void)
 {
@@ -52,34 +51,29 @@ void main(void)
 	int V1; // processed signal from lead 1
 	int V2; // processed signal from lead 2
 	int EMG; // decoded result
-	int count;
 
     //Initialize
     SysInit();
+	Calibrate();
     LCDClear();
-    Calibrate();
 
     // EMG Decoder Loop
     while(1) {
 		rawV1 = GetData(1); // Acquire voltage from channel 1
 		rawV2 = GetData(2); // Acquire voltage from channel 2
 		V1=Debounce(rawV1, oldRawV1,oldV1,1); //Process voltage from channel 1
-		oldRawV1 = rawV1;
+		oldRawV1=rawV1;
 		oldV1=V1;
 		V2=Debounce(rawV2,oldRawV2,oldV2,2); //Process voltage from channel 2
-		oldRawV2 = rawV2;
+		oldRawV2=rawV2;
 		oldV2=V2;
+		if( ((oldV1==0 && V1==1) || (oldV2==0 && V2==1)) && !(V1==1 && V2==1)){ // need to debounce simultaneous contractions
+			Delay10KTCYx(1);  // Delay 10 ms
+			continue;
+		}
 		EMG=Decode(V1,V2); // Decode
         Transmit(EMG); // Print value to screen or communication
 		Delay10KTCYx(t);  // Delay t/100 seconds
-		/*
-        // For Communication Testing
-		TXREG1=60; // Set info to be transmitted
-		Delay10KTCYx(50); // Delay 2 seconds        
-		recComm=RCREG1; // Read info received       
-		LCDGoto(0,0);
-		LCDPutByte(recComm);
-		*/
 	};
 }
 
@@ -132,83 +126,117 @@ void SysInit(void)
 	oldV1=0;
 	oldV2=0;
 	state=0;
-	thres1=0;
-	thres2=0;
 
 	//Configure variables (adjustable)
     //thres=300; //Initial threshold
-    // hyst=100; //Hysteresis width
+    //hyst=100; //Hysteresis width
 	t=20; //200 ms (1 corresponds to 10 ms)
 }
 
+//Calibration period to adjust for user/muscle activity
 void Calibrate(void){
-	int count;
-	int ca1;
-	int ca2;
+	int i;
+	int old1;
+	int old2;
+	int raw1;
+	int raw2;
+	int max1;
+	int min1;
+	int max2;
+	int min2;
+	int slope;
+	int rising1;
+	int descend1;
+	int rising2;
+	int descend2;
     char str[4];
-    //Calibration period
-    // Display EMG
+    // Initialize calibration
 	LCDGoto(5,0);
 	LCDPutByte(0);
     Delay10KTCYx(1000); //Wait 10 seconds for system to settle
+	old1=0;
+	old2=0;
     max1=0;
     min1=1023;
     max2=0;
     min2=1023;
-    // Display EMG
+	rising1=0;
+	descend1=0;
+	rising2=0;
+	descend2=0;
+    // Calibrate mag and slope thresholds
 	LCDGoto(5,0);
 	LCDPutByte(8);
-    for(count=0;count<200;count++){ //Calibrate for 10 seconds
-    	//Ch1
-    	ca1=GetData(1); // Acquire voltage from channel 1
-    	if(ca1>max1){
-    		max1=ca1;
+    for(i=0;i<200;i++){ //10 seconds of calibration data
+    	//Channel 1 magnitude calibration
+    	raw1=GetData(1); // Acquire voltage from channel 1
+    	if(raw1>max1){
+    		max1=raw1;
     	}
-    	if(ca1<min1){
-    		min1=ca1;
+    	if(raw1<min1){
+    		min1=raw1;
     	}
-    	//Ch2
-		ca2=GetData(2); // Acquire voltage from channel 2
-    	if(ca2>max2){
-    		max2=ca2;
+		// slope calibration
+		slope = raw1-old1;
+		if(slope>rising1)
+			rising1 = slope;
+		if(slope<descend1)
+			descend1 = slope;
+		old1 = raw1;
+    	//Channel 2 magnitude calibration
+		raw2=GetData(2); // Acquire voltage from channel 2
+    	if(raw2>max2){
+    		max2=raw2;
     	}
-    	if(ca2<min2){
-    		min2=ca2;
-    	}
+    	if(raw2<min2){
+    		min2=raw2;
+		}
+		// slope calibration
+		slope = raw2-old2;
+		if(slope>rising2)
+			rising2 = slope;
+		if(slope<descend2)
+			descend2 = slope;
+		old2 = raw2;
+		//Display raw value for Ch1
         LCDGoto(8,0);
-		sprintf(str,"%04u",ca1);
+		sprintf(str,"%04u",raw1);
         LCDPutChar(str[0]);
         LCDPutChar(str[1]);
         LCDPutChar(str[2]);
         LCDPutChar(str[3]);
-        //Raw value for Ch1
+        //Display raw value for Ch2
         LCDGoto(8,1);
-        sprintf(str,"%04u",ca2); 
+        sprintf(str,"%04u",raw2); 
         LCDPutChar(str[0]);
         LCDPutChar(str[1]);
         LCDPutChar(str[2]);
         LCDPutChar(str[3]);
-    Delay10KTCYx(5);
+		Delay10KTCYx(t);
     }
     LCDGoto(5,0);
 	LCDPutByte(1);
-    //need to figure this out
+    //Need to figure this out
     thres1=min1+(max1-min1)*3/10;
     thres2=min2+(max2-min2)*3/10;
-    //Raw value for Ch1
-    	LCDGoto(0,0);
-		sprintf(str,"%04u",thres1);
-        LCDPutChar(str[0]);
-        LCDPutChar(str[1]);
-        LCDPutChar(str[2]);
-        LCDPutChar(str[3]);
-        //Raw value for Ch1
-        LCDGoto(0,1);
-        sprintf(str,"%04u",thres2); 
-        LCDPutChar(str[0]);
-        LCDPutChar(str[1]);
-        LCDPutChar(str[2]);
-        LCDPutChar(str[3]);
+	rslope1 = rising1*0.9;
+	dslope1 = descend1*0.9;
+	rslope2 = rising2*0.9;
+	dslope2 = descend2*0.9;
+    //Ch1 mag thres
+    LCDGoto(0,0);
+	sprintf(str,"%04u",thres1);
+    LCDPutChar(str[0]);
+    LCDPutChar(str[1]);
+    LCDPutChar(str[2]);
+    LCDPutChar(str[3]);
+    //Ch2 mag thres
+	LCDGoto(0,1);
+    sprintf(str,"%04u",thres2); 
+    LCDPutChar(str[0]);
+    LCDPutChar(str[1]);
+    LCDPutChar(str[2]);
+    LCDPutChar(str[3]);
 }
 
 // ADC sampling of EMG leads
@@ -231,35 +259,39 @@ int GetData(int channel) {
 		volt=ADRESH;
 		volt=(volt<<8) | ADRESL; //Make 10-bit
 	}
-        LCDGoto(8,channel-1);
-        sprintf(str,"%04u",volt); 
-        LCDPutChar(str[0]);
-        LCDPutChar(str[1]);
-        LCDPutChar(str[2]);
-        LCDPutChar(str[3]);
+	LCDGoto(8,channel-1);
+	sprintf(str,"%04u",volt); 
+	LCDPutChar(str[0]);
+	LCDPutChar(str[1]);
+    LCDPutChar(str[2]);
+    LCDPutChar(str[3]);
 	return volt;
 }
 
 // Process signal from each channel into either high or low
 int Debounce(int raw, int oldraw, int olddeb, int channel){
     int thres;
-	float slope;
-	float slopeThres;
+	int slope;
+	int risThres;
+	int desThres;
 	//Set threshold
-	if(channel=1){
+	if(channel==1){
 		thres=thres1;
+		risThres=rslope1;
+		desThres=dslope1;
 	}
-	if(channel=2){
+	if(channel==2){
 		thres=thres2;
+	    risThres=rslope2;
+		desThres=dslope2;
 	}
-
 	// Find instantaneous slope
-	slope = (raw-oldraw)/(t*10.0); // unit: voltage units/ms
+	slope = raw-oldraw; // unit: voltage units
 	// Threshold is 15 V/s * 1023 units/5V = 3069 units/sec = 3.069 units/ms
-	slopeThres = 3.069;
+	// slopeThres = 3.069;
 	// If the signal was low before:
 	if (olddeb==0){
-		if ((slope>slopeThres) || (raw>thres)){
+		if ((slope>risThres) || (raw>thres)){
 			return 1;
 		}
 		else{
@@ -268,7 +300,7 @@ int Debounce(int raw, int oldraw, int olddeb, int channel){
 	}
 	// If the level was high before:
     if (olddeb==1){
-		if((slope<0 && raw<thres)){ //(slope<-1*slopeThresh) || (slope<0 && raw<Thresh)
+		if((slope<0 && raw<thres)){ //(slope<desThres) || (slope<0 && raw<Thresh)
             return 0;
         }
         else{
@@ -303,6 +335,6 @@ void Transmit(int info){
 	LCDGoto(13,1);
 	LCDPutByte(info);
 	// Transmit EMG
-	TXREG1 = info;
+	TXREG1 = info; // Set info to be transmitted
 }
 
