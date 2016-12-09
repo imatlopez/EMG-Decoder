5953befbbeeaf16b1a4133a373483966f6459a94
Matias Lopez <imatlopez@gmail.com>
2016-12-09 10:36:38 -0500

    ANOVA

--- Software/SpecAutom/perfAnyl.m
+++ Software/SpecAutom/perfAnyl.m
@@ -1,4 +1,4 @@
-clear;
+clear; close all;
 %% Import the data
 [~, ~, raw] = xlsread('../../Forms/Testing.xlsx','Analysis');
 dat = cell2mat(raw(2:end,3:23));
@@ -25,19 +25,23 @@ xlabel('Gain Resistance (K\Omega)')
 ylabel('False Positive Rate (%)')
 title('False Positive for Increasing Gain Resistance')
 fixplot
+print -dpng SpecPlot
 
 %% Lilliefors
-N = zeros(3,7,2);
+N = zeros(6,7);
 V(1) = vartestn(P(:,:,1));
 V(2) = vartestn(P(:,:,2));
 for i = 1:3
     for j = 1:7
         for k = 1:2
-            [~, N(i,j,k)] = lillietest(P((1:5)+(i-1)*5,j,k));
+            [~, N(i+3*k-3,j)] = lillietest(P((1:5)+(i-1)*5,j,k));
         end
     end
 end
 
 %% 2 Way Anova
+close all;
 [H1, T1, S1] = anova2(P(:,:,1),5);
+set(gcf, 'rend','painters','pos',[10 10 360 120]); print -dpng Anv1
 [H2, T2, S2] = anova2(P(:,:,2),5);
+set(gcf, 'rend','painters','pos',[10 10 360 120]); print -dpng Anv2
